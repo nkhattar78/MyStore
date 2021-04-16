9555a422b4e9cafea4ce651566ddd04b407ca193
nkhattarMS <72445643+nkhattarMS@users.noreply.github.com>
2021-04-16 17:31:55 +0530

    correcting the DB string

--- helpers/initdb.js
+++ helpers/initdb.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
 function initDB() {
+    //mongoose.connection.close();    
     console.log('Number of DB connections: ' + mongoose.connections.length )
     if (mongoose.connections[0].readyState) {
         console.log("DB already connected");
         return;        
     }
 
-    mongoose.connect('mongodb://127.0.0.1:27017/storedb', { useNewUrlParser: true, useUnifiedTopology: true});
-    //mongoose.connect('mongodb+srv://admin:Namaste78@testcluster.n3ium.mongodb.net/storedb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
-    //mongodb+srv://admin:Namaste78@testcluster.n3ium.mongodb.net/storedb?retryWrites=true&w=majority
+    //mongoose.connect('mongodb://127.0.0.1:27017/storedb', { useNewUrlParser: true, useUnifiedTopology: true});
+    //mongoose.connect('mongodb+srv://nkhattar:F8pv1dqOxWu4xuRK@cluster0.ai6zo.mongodb.net/storedb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb://nkhattar:Namaste@clusternk-shard-00-00.ai6zo.mongodb.net:27017,clusternk-shard-00-01.ai6zo.mongodb.net:27017,clusternk-shard-00-02.ai6zo.mongodb.net:27017/storedb?ssl=true&replicaSet=atlas-4qz547-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
     mongoose.connection.on('connected', ()=> {
         console.log("DB connected")
     })
