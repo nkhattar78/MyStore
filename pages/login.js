cf079256721ccb40b6d25617a5d867788e1bb381
nkhattarMS <72445643+nkhattarMS@users.noreply.github.com>
2021-04-18 15:50:06 +0530

    Adding Access Conttoil in headers

--- pages/login.js
+++ pages/login.js
@@ -18,6 +18,7 @@ const Login = ()=> {
             headers:{
                 'Content-Type':'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
             },
             body:JSON.stringify({
                 email,
